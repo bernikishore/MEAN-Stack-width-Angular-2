ae5f24d2ae05de8904e20e8acaef94fbcf42a6a0
karthikeyan <karthikeyan.indran@ziffity.com>
2017-11-21 18:46:01 +0530

    body-parser & bcrypt npm install username email password validation using postman

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+
+const router = express.Router();
 const mongoose = require('mongoose');
 const config = require('./config/databse');
 const path = require('path');
+const authentication = require('./routes/authentication')(router);
+const bodyParser = require('body-parser');
+
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, { useMongoClient: true }, (err) => {
@@ -14,7 +20,16 @@ mongoose.connect(config.uri, { useMongoClient: true }, (err) => {
 });
 
 
+//parse applicaption/x-www.form-urlencoded
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+
+app.use(bodyParser.json());
+
 app.use(express.static(__dirname + '/client/dist/'));
+app.use('/authentication', authentication);
 
 /* app.get('*', (req, res) => {
     res.send('<h2>Hello World</h2>');
@@ -23,7 +38,7 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/dist/index.html'));
 });
 
-app.listen(8080, () => {
+app.listen(3000, function() {
     console.log(config.secret);
-    console.log('Listening port 8080');
-})
+    console.log('Listening port 3000');
+});
---
+++ models/user.js
@@ -0,0 +1,143 @@
+var mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+var Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
+
+let emailLengthChecker = (email) => {
+    if (!email) {
+        return false;
+    } else {
+        if (email.length < 5 || email.length > 30) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
+
+let validEmail = (email) => {
+    if (!email) {
+        return false;
+    } else {
+        const regExp = new
+        RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        return regExp.test(email);
+    }
+};
+
+let usernameLengthChecker = (username) => {
+    if (!username) {
+        return false;
+    } else {
+        if (username.length < 3 || username.length > 30) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
+
+let validUsername = (username) => {
+    if (!username) {
+        return false;
+    } else {
+        const regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+        return regExp.test(username);
+    }
+};
+
+let passwordLengthChecker = (password) => {
+    if (!password) {
+        return false;
+    } else {
+        if (password.length < 8 || password.length > 25) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+};
+
+let validPassword = (password) => {
+    if (!password) {
+        return false;
+    } else {
+        const regExp = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,25})");
+        return regExp.test(password);
+    }
+};
+
+const passwordValidators = [{
+    validator: passwordLengthChecker,
+    message: 'Password must be atleast 5 character but no more than 25'
+}, {
+    validator: validPassword,
+    message: 'Password must be One uppercase lowercase special character'
+}];
+
+const usernameValidators = [{
+    validator: usernameLengthChecker,
+    message: 'Username must be at least 5 character but no more than 30'
+}, {
+    validator: validUsername,
+    message: 'User name must not be Special character'
+}];
+
+
+const emailValidators = [{
+    validator: emailLengthChecker,
+    message: 'Email must be at least 5 character but no more than 30'
+}, {
+    validator: validEmail,
+    message: 'must be a valid email'
+}];
+
+
+
+
+
+
+/* const regExp = new RegExp(/^[a-zA-Z0-9]+$/);
+return regExp.test(username);
+const regExp = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,25})");
+return regExp.test(password); */
+
+var userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        validate: emailValidators,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        validate: usernameValidators
+    },
+    password: {
+        type: String,
+        required: true,
+        validate: passwordValidators
+    }
+});
+
+
+userSchema.pre('save', function(next) {
+    if (!this.isModified('password'))
+        return next();
+
+    bcrypt.hash(this.password, null, null, (err, hash) => {
+        if (err) return next(err);
+        this.password = hash;
+        next();
+    });
+});
+
+userSchema.methods.comparePassword = (password) => {
+    return bcrypt.compareSync(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
